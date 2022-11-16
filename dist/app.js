424ee3e68730a3f1a694c39b39c4d9e4a944e733
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2022-11-16 15:32:37 -0300

    Added generateTodoHtml function to avoid code duplication

--- dist/app.js
+++ dist/app.js
@@ -42,57 +42,65 @@ function updateDate() {
   dateYearElement.textContent = year;
 }
 
+function generateTodoHtml(todoInputValue) {
+  const todoHtml = `
+  <li class="todo-element">
+  <div class="todo-title">${todoInputValue}</div>
+          <div class="todo-btns-box">
+          <button class="btn todo-btn--finish-task">
+          <svg
+          class="todo-btn--finish-task-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+                >
+                <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+                </svg>
+                </button>
+                <button class="btn todo-btn--delete-task">
+                <svg
+                class="todo-btn--delete-task-icon"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+                >
+                <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+                />
+                </svg>
+                </button>
+                </div>
+                </li>
+                `;
+  return todoHtml;
+}
+
 function addTodoElement(event) {
   event.preventDefault();
 
-  const todoHtml = `
-    <li class="todo-element">
-    <div class="todo-title">${todoFormInputElement.value}</div>
-            <div class="todo-btns-box">
-            <button class="btn todo-btn--finish-task">
-            <svg
-            class="todo-btn--finish-task-icon"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-6 h-6"
-                  >
-                  <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                  </svg>
-                  </button>
-                  <button class="btn todo-btn--delete-task">
-                  <svg
-                  class="todo-btn--delete-task-icon"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-6 h-6"
-                  >
-                  <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                  />
-                  </svg>
-                  </button>
-                  </div>
-                  </li>
-                  `;
-
+  // Adds todo if input is not empty
   if (todoFormInputElement.value !== "" && todoFormInputElement.value !== " ") {
-    todoListElement.insertAdjacentHTML("beforeend", todoHtml);
+    todoListElement.insertAdjacentHTML(
+      "beforeend",
+      generateTodoHtml(todoFormInputElement.value)
+    );
 
-    // Adding todo to local storage
+    // Adds todo to local storage
     saveLocalTodos(todoFormInputElement.value);
 
+    // Clears input
     todoFormInputElement.value = "";
   }
 }
@@ -141,48 +149,7 @@ function getLocalStorageTodos() {
   }
 
   todos.forEach((todo) => {
-    const todoHtml = `
-    <li class="todo-element">
-    <div class="todo-title">${todo}</div>
-            <div class="todo-btns-box">
-            <button class="btn todo-btn--finish-task">
-            <svg
-            class="todo-btn--finish-task-icon"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-6 h-6"
-                  >
-                  <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                  </svg>
-                  </button>
-                  <button class="btn todo-btn--delete-task">
-                  <svg
-                  class="todo-btn--delete-task-icon"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-6 h-6"
-                  >
-                  <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                  />
-                  </svg>
-                  </button>
-                  </div>
-                  </li>
-                  `;
-    todoListElement.insertAdjacentHTML("beforeend", todoHtml);
+    todoListElement.insertAdjacentHTML("beforeend", generateTodoHtml(todo));
   });
 }
 
